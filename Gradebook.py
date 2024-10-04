67be8d6894c02d93c7ac17c47d3eaae658f44d42
Dia <142447820+marksofdespair@users.noreply.github.com>
2024-10-04 15:15:42 -0500

    Create Gradebook.py

---
+++ Gradebook.py
@@ -0,0 +1,24 @@
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Your code below: 
+subjects = ["physics", "calculus", "poetry", "history"]
+grades = [98, 97, 85, 88]
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
+
+print(gradebook)
+
+gradebook.append(["computer science", 100])
+gradebook.append(["visual arts", 93])
+
+print (gradebook)
+# Locate the sublist for "poetry"
+poetry_entry = gradebook[2]
+
+# Use .remove() to remove the grade value (85)
+poetry_entry.remove(85)
+poetry_entry.append("Pass")
+print(gradebook)
+
+full_gradebook = gradebook + last_semester_gradebook
+
+print(gradebook)

53fed6a37b0c5a9468c949833d4c00fe59aff47c
Dia <142447820+marksofdespair@users.noreply.github.com>
2024-10-10 12:18:50 -0500

    Update Loop.py

--- Loop.py
+++ Loop.py
@@ -46,11 +46,18 @@ for student in students_period_A:
 
 # ---------------------------------------------------------------------------------
 
+# Uses a for loop to iterate through the dog_breeds_available_for_adoption list and print() out each dog breed.
+# Uses the <temporary variable> name of dog_breed in your loop declaration.
+
 dog_breeds_available_for_adoption = ["french_bulldog", "dalmatian", "shihtzu", "poodle", "collie"]
 dog_breed_I_want = "dalmatian"
+
+# Inside the for loop, after you print each dog breed, check if the current element inside dog_breed is equal to dog_breed_I_want. If so, print "They have the dog I want!"
 
 for dog_breed in dog_breeds_available_for_adoption:
   print(dog_breed)
   if dog_breed == dog_breed_I_want:
     print("They have the dog I want!")
     break
+
+# Add a break statement when your loop has found dog_breed_I_want so that the rest of the list does not need to be checked once we have found our breed.
